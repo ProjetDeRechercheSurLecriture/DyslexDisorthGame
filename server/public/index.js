6ed7c0e892562a701dc29411bc9a7d077f0d42a4
cesine <cesine@yahoo.com>
2012-10-29 13:19:49 -0400

    created html pages for the results and prepareing to switch to a new html5 framework for #49 #48 #47

---
+++ server/public/index.js
@@ -0,0 +1,10 @@
+load app
+
+if no app, show them the buttons
+* tutorials
+* Begin testing
+
+
+if begin testing, create a full app with fake organization and fake experimenter and normal participant, hide menu, show tests
+
+click on test, save app and go to test. if teset opens and no app, return to main menu with error.
