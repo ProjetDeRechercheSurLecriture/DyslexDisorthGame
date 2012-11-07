8c1eb0554f36ef664127c85d67f73013f79e2c4c
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-07 16:01:47 -0500

    Expanded jsons; no other major changes.

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -7,13 +7,23 @@ function SessionListCtrl($scope, $http) {
     $scope.sessions = data;
   });
   
+  $http.get('data/child.json').success(function(data) {
+	$scope.childs = data;
+  });
+  
   $scope.orderProp = 'participantID';
+  
+  
+  //GET RID OF THIS AND REFACTOR USING ROUTES
+  $scope.displayReport = function(sessionID) {
+	  document.getElementById('span-sessionID').innerHTML=sessionID;
+	  document.getElementById('angularTest').innerHTML="TEST";
+	  var data = $http.get('data/session_data.json');
+	  data = JSON.parse(data);
+	  window.alert(data[0].participantID);
+	  
+	  
+  }
 }
-
-function ChildListCtrl($scope, $http) {
-	$http.get('data/child.json').success(function(data) {
-		$scope.childs = data;
-	});
-}	
 
 //PartcipantListCtrl.$inject = ['$scope', '$http'];
