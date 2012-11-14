56b488b39e098b5d40744963da4741c23af5cb69
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-14 15:32:52 -0500

    Fixes #53 Search results are now paginated.

--- client/app/js/filters.js
+++ client/app/js/filters.js
@@ -2,8 +2,21 @@
 
 /* Filters */
 
-angular.module('headphoneFilter', []).filter('checkmark', function() {
+angular.module('searchFilters', []).
+filter('checkmark', function() {
 	return function(input) {
 		return input==1 ? '\u2713' : '\u2718';
 	};
+}).
+filter('startFrom', function() {
+	return function(input, start) {
+		if (input == undefined) {
+			return;
+		}
+		else {
+			start = +start; //parse to int
+			return input.slice(start);
+		}
+	}
 });
+
