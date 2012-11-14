13d060829f670e24f5e434d5a31ae26ebe804fec
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-14 12:21:59 -0500

    Further fixes #55 App now has a saveChanges() function that updates CouchDB.

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-function SessionListCtrl($scope, $resource, Child, Session, GetNewUUID, PostToCouch) {
+function MainCtrl($scope, $resource, Child, Session, GetNewUUID, AccessCouch) {
 
 //Query data; assign to template scope
 	
@@ -27,45 +27,12 @@ function SessionListCtrl($scope, $resource, Child, Session, GetNewUUID, PostToCo
 	        noresultsdiv.style.display = 'none';
 	    }
 	};
-	
-/* REWRITE this function to add/modify a single record */
-		
-	$scope.pushToCouch = function (dataToPost)	{
-		var NewUUID;
-		var pushToURL;
-		var recordToPush;
-		var tempData
-		var i = 0;
-		var interval = setInterval(function(){
-			(function(i) {
-				NewUUID = GetNewUUID.query(function() {
-					tempData = dataToPost[i];
-					delete tempData.$$hashKey;
-					recordToPush = JSON.stringify(tempData);
-					PostToCouch.save({UUID: NewUUID.uuids}, recordToPush);
-				});
-			}(i));
-			i++;
-			if (i >= dataToPost.length) {
-			clearInterval(interval);
-			}
-		}, 1000);
-	}	
-
-/* END rewrite */	
-}
-
-function SessionReportCtrl($scope, $routeParams) {
-
-//Limit value of sessionID in template to value of sessionID in routeParams
-	
-	$scope.filterProp = $routeParams.sessionID;
 
 //Show/hide Edit/Cancel buttons; make template content (non)editable	
 	
 	$scope.toggleEditButtons = function() {
-		var descriptionField = document.getElementById('description_text');
-		var discussionField = document.getElementById('discussion_text');
+		var descriptionField = document.getElementById('descriptionEdit');
+		var discussionField = document.getElementById('discussionEdit');
 		var editButton = document.getElementById('edit');
 		var cancelSaveButton = document.getElementById('cancel_save');
 
@@ -80,56 +47,44 @@ function SessionReportCtrl($scope, $routeParams) {
 			discussionField.contentEditable = 'false';
 			editButton.style.display = 'block';
 			cancelSaveButton.style.display = 'none';
-			document.location.reload(true);
 		};
 	};
+	
+//Save changes made to edited fields; push changes to CouchDB
+//NOTE: all HTML ids must match db ids exactly; first argument must always be UUID of current db document	
 
-/* REWRITE this function to use CouchDB */	
+/*Add some sort of spinner while saving*/
 	
-//Save (edited) template content to local storage; key = sessionID
+	$scope.saveChanges = function() {
+		var itemsToEdit = arguments;
+		var currentUUID = itemsToEdit[0];
+		var updatedRecord = AccessCouch.query({UUID: currentUUID}, function(){
+			for (var i = 1; i < itemsToEdit.length; i++) {		
+				var dataToPost = document.getElementById(itemsToEdit[i]).innerHTML;
+				var itemID = itemsToEdit[i];
+				updatedRecord[itemID] = dataToPost;
+			}
+			updatedRecord.$save();
+			location.reload();
+		});			
+	}
 	
-	$scope.saveChanges = function(editedSession) {
-		var descriptionText = document.getElementById('description_text').innerHTML;
-		var discussionText = document.getElementById('discussion_text').innerHTML;
-		editedSession = {"id": editedSession, "discussionEditedText" : discussionText, "descriptionEditedText" : descriptionText};
-		localStorage.setItem(editedSession.id, JSON.stringify(editedSession));
-	};
+}
+
+function SessionReportCtrl($scope, $routeParams) {
+
+//Set template filter value to value of sessionID in routeParams
+	
+	$scope.filterProp = $routeParams.sessionID;
 	
 }
 
 function ParticipantReportCtrl($scope, $routeParams) {
 
-//Limit value of participantID in template to value of participantID in routeParams	
+//Set template filter value to value of participantID in routeParams	
 	
 	$scope.filterProp = $routeParams.participantID;
 
 }
 
-/* REWRITE this function to use CouchDB */
-
-//Check to see if the description and discussion fields in the session
-//have been updated (saved in localStorage); Call this controller inside
-//ng-repeat to ensure a unique sessionID
-
-function CheckEditableFieldsCtrl($scope) {
-	var currentSession = $scope.session.sessionID;
-	var storedID = JSON.parse(localStorage.getItem(currentSession));
-	try {
-		if (storedID.descriptionEditedText != undefined) {
-			$scope.session.descriptionEdit = storedID.descriptionEditedText;
-		}
-	}
-	catch(e) {
-		return
-	};
-	try {
-		if (storedID.discussionEditedText != undefined) {
-			$scope.session.discussionEdit = storedID.discussionEditedText;
-		}
-	}
-	catch(e) {
-		return
-	};
-
-}
 //PartcipantListCtrl.$inject = ['$scope', '$http'];
