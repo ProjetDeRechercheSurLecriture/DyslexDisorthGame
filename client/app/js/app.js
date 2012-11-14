748d8c5bc9a185b502aa2d97ac3a71a8f685061d
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-14 18:03:19 -0500

    Minor cosmetic updates, but see #57 IMPORTANT.

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -5,7 +5,7 @@
 angular.module('participantData', ['searchFilters', 'phophloServices']).
 	config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/sessions', {templateUrl: 'partials/search_results.html',   controller: MainCtrl}).
+		when('/sessions', {templateUrl: 'partials/results_main.html',   controller: MainCtrl}).
 		when('/sessions/:sessionID', {templateUrl: 'partials/session_report.html', controller: SessionReportCtrl}).
 		when('/participants/:participantID', {templateUrl: 'partials/participant_report.html', controller: ParticipantReportCtrl}).
 		otherwise({redirectTo: '/sessions'});
