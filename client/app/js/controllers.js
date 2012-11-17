4d6bc0d6502746e900b67d7b80ebea11bd115ec6
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-17 09:56:37 -0500

    Added new user function; TODO fix save feature using ng-model.

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -11,7 +11,7 @@ function MainCtrl($scope, $resource, Participant, Session, GetNewUUID, AccessCou
 	$scope.orderProp = 'participantID';
 	$scope.searching = 'true';
 	$scope.editing = 'false';
-
+	
 //Test to see if text in search box returns any results and hide/display divs accordingly	
 	
 	$scope.displaySearchResults = function(resultsCount) {	
@@ -33,6 +33,8 @@ function MainCtrl($scope, $resource, Participant, Session, GetNewUUID, AccessCou
 //NOTE: all HTML ids must match db ids exactly; function('elementID', 'elementID', ...)
 	
 	$scope.editRecords = function() {
+		$scope.$broadcast('event:force-model-update');
+		$scope.$broadcast
 		$scope.editing = 'true';
 		for (var i = 0; i < arguments.length; i++) {
 			document.getElementById(arguments[i]).contentEditable = 'true';
@@ -54,6 +56,8 @@ function MainCtrl($scope, $resource, Participant, Session, GetNewUUID, AccessCou
 //NOTE: all HTML ids must match db ids exactly; function(UUID, 'elementID', 'elementID', ...)	
 
 /*Add some sort of spinner while saving*/
+
+/*Investigate a cleaner method to do this with ng-models*/	
 	
 	$scope.saveRecords = function() {
 		var itemsToEdit = arguments;
@@ -70,6 +74,34 @@ function MainCtrl($scope, $resource, Participant, Session, GetNewUUID, AccessCou
 			});
 		});			
 	}	
+
+//Start test
+	$scope.testSave = function(currentUUID, data) {
+		var updatedRecord = AccessCouch.query({UUID: currentUUID}, function() {
+			
+//			for (var i = 1; i < itemsToEdit.length; i++) {		
+//				document.getElementById(itemsToEdit[i]).contenteditable = 'false';
+//			}
+			window.alert(JSON.stringify(data));
+//				window.location.reload();
+		});
+//		window.alert(JSON.stringify(data));
+			
+		
+		
+		
+//		AccessCouch.save(edits, function() {	
+//				window.alert($scope.data.firstName);
+//				window.alert(JSON.stringify(edits));
+//			});
+			
+	}
+	
+//End test
+
+
+
+
 
 };
 
@@ -89,12 +121,50 @@ function ParticipantReportCtrl($scope, $routeParams) {
 
 };
 
-function NewUserCtrl($scope, Participant) {
-	$scope.data = Participant.query();
+function NewUserCtrl($scope, Participant, AccessCouch) {
 	
-	$scope.createNewUser = function() {
-		window.alert('Success!');
+	$scope.cancel = function() {
+		location.reload();
+	}
+	
+	$scope.createNewUser = function(data) {
+			var currentParticipantIDs = Participant.query(function() {
+				var newParticipantID = generateNewParticipantID(currentParticipantIDs);
+				if (newParticipantID == undefined) {
+					window.alert("Please try again.") //should this just call generateNewParticipantID() again? 
+				}
+				else {
+					data.JSONType = "participant";
+					data.participantID = newParticipantID;
+					var newUser = AccessCouch.save(data, function() {
+						window.alert("New user created. New user Participant ID: " + newParticipantID);
+						window.location = "index.html";
+					});
+				}
+			});
+			
 	};
 };
+
+function generateNewParticipantID(data) {
+	var randomParticipantID=Math.floor(Math.random()*(999999 - 100000 + 1)) + 100000;
+	var duplicatedID;
+	//Make sure randomly generated ID doesn't already exist
+	for (var i = 0; i < data.rows.length; i++) {
+		if (randomParticipantID == data.rows[i].value.participantID) {
+			duplicatedID = 1;
+		}
+		else {
+			duplicatedID = 0;
+		}
+	}
+	if (duplicatedID == 0) {
+		return randomParticipantID;
+	}
+	else {
+		return;	
+	}
+}
+
 
 //PartcipantListCtrl.$inject = ['$scope', '$http'];
