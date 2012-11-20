30e7e939f86cec3660af62ecd6fd2b1c214c88bb
Tobin Skinner <tobin.skinner@gmail.com>
2012-11-20 11:16:24 -0500

    Updated search to show/hide divs more sensibly.

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -12,4 +12,36 @@ angular.module('PhoPhloApp', ['searchFilters', 'phophloServices']).
 		when('/sessions/:sessionID', {templateUrl: 'partials/session_report.html', controller: SessionReportCtrl}).
 		when('/participants/:participantID', {templateUrl: 'partials/participant_report.html', controller: ParticipantReportCtrl}).
 		otherwise({redirectTo: '/test'});
-}]);
+}]).
+directive('onFocus', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            elm.bind('focus', function() {
+//                scope.query = "";
+            	scope.$apply(attrs.onFocus);                
+            });
+        }
+    };        
+}).
+directive('onBlur', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            elm.bind('blur', function() {
+                scope.$apply(attrs.onBlur);
+            });
+        }
+    };        
+}).
+directive('ngEnter', function() {
+    return function(scope, elm, attrs) {
+        elm.bind('keypress', function(e) {
+            if (e.charCode === 13) {
+            	scope.$apply(attrs.ngEnter);
+            	elm[0].blur();
+            };
+            
+        });
+    };
+});;
