7e6c80b71edbcc434b45006b7fe706be913f3b8f
Tobin Skinner <tobin.skinner@gmail.com>
2013-01-31 17:56:32 -0500

    File cleanup.

--- client/app/modules/phophlo/filters/PhoPhloFilters.js
+++
@@ -1,7 +0,0 @@
-console.log("Loading the PhoPhloFilters.");
-
-'use strict';
-define([ "angular" ], function(angular) {
-	var PhoPhloFilters = angular.module('PhoPhlo.filters', []);
-	return PhoPhloFilters;
-});
--- client/app/modules/phophlo/services/PhoPhloServices.js
+++
@@ -1,7 +0,0 @@
-console.log("Loading the PhoPhloServices.");
-
-'use strict';
-define([ "angular" ], function(angular) {
-	var PhoPhloServices = angular.module('PhoPhlo.services', [ 'ngResource' ]);
-	return PhoPhloServices;
-});
