eefdb0ae72a58363b2c9de059993629e65e3b2f3
Tobin Skinner <tobin.skinner@gmail.com>
2013-02-01 15:25:33 -0500

    Cleaned up directives to make syntax more uniform.

--- client/app/modules/sails/directives/SAILSDirectives.js
+++ client/app/modules/sails/directives/SAILSDirectives.js
@@ -4,8 +4,8 @@ console.log("Loading the SAILSDirectives.");
 define([ "angular" ], function(angular) {
   var SAILSDirectives = angular.module('SAILS.directives', []).directive(
       'moduleVersion', [ 'version', function(version) {
-        return function(scope, elm, attrs) {
-          elm.text(version);
+        return function(scope, element, attrs) {
+          element.text(version);
         };
       } ]).directive(
       'stimuli2',
@@ -69,26 +69,26 @@ define([ "angular" ], function(angular) {
       }).directive("coordinatesClick", function(mouse) {
 
     // Connect the Angular context to the DOM events.
-    var linkFunction = function($scope, $element, $attributes) {
+    var linkFunction = function(scope, element, attrs) {
 
       // Get the expression we want to evaluate on the
       // scope when the document is clicked.
-      var scopeExpression = $attributes.coordinatesClick;
+      var scopeExpression = attrs.coordinatesClick;
 
       // Bind to the element click event.
-      $element.on("click", function(event) {
+      element.on("click", function(event) {
 
         // Set the click coordinates in the mouse
         // service.
         mouse.setLocation(event.pageX, event.pageY);
-        var imageX = event.pageX - $element[0].offsetLeft;
-        var imageY = event.pageY - $element[0].offsetTop;
-        console.log("Results: " + "\npageX: " + event.pageX + "\npageY: " + event.pageY + "\nimageX: " + imageX + "\nimageY: " + imageY + "\nimage clicked: " + $element[0].id);
+        var imageX = event.pageX - element[0].offsetLeft;
+        var imageY = event.pageY - element[0].offsetTop;
+        console.log("Results: " + "\npageX: " + event.pageX + "\npageY: " + event.pageY + "\nimageX: " + imageX + "\nimageY: " + imageY + "\nimage clicked: " + element[0].id);
         
         // Apply the scope expression so the
         // handler is invoked and the digest()
         // method is invoked implicitly.
-        $scope.$apply(scopeExpression);
+        scope.$apply(scopeExpression);
       });
 
     };
