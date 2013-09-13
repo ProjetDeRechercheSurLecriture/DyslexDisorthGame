f789e9b9d67a3a31d0e5c03f47a80a8ef1329032
cesine <cesine@yahoo.com>
2013-09-12 22:24:17 -0400

    android test project

--- copy_assets_from_www_to_platforms.sh
+++ copy_assets_from_www_to_platforms.sh
@@ -10,6 +10,16 @@ cp www/res/icon/android/icon-48-mdpi.png platforms/android/res/drawable-mdpi/ico
 cp www/res/icon/android/icon-72-hdpi.png platforms/android/res/drawable-hdpi/icon.png
 cp www/res/icon/android/icon-96-xhdpi.png platforms/android/res/drawable-xhdpi/icon.png
 
+mkdir platforms/android-test/res/drawable-ldpi
+mkdir platforms/android-test/res/drawable-mdpi
+mkdir platforms/android-test/res/drawable-hdpi
+mkdir platforms/android-test/res/drawable-xhdpi
+
+cp www/res/icon/android/icon-36-ldpi.png platforms/android-test/res/drawable-ldpi/icon.png
+cp www/res/icon/android/icon-48-mdpi.png platforms/android-test/res/drawable-mdpi/icon.png
+cp www/res/icon/android/icon-72-hdpi.png platforms/android-test/res/drawable-hdpi/icon.png
+cp www/res/icon/android/icon-96-xhdpi.png platforms/android-test/res/drawable-xhdpi/icon.png
+
 echo "Copying icons from www to ios"
 mkdir platforms/ios/MobileEmbeddedDemos/Resources/icons
 cp www/res/icon/ios/icon-57.png platforms/ios/MobileEmbeddedDemos/Resources/icons/icon.png
