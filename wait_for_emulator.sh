5fa9a2be382dd2cfafcc910e224df2edce7c8403
cesine <cesine@yahoo.com>
2013-10-03 20:10:15 -0400

    moved shell prep out of travis and into a seperate script so it can be used more generally

--- wait_for_emulator.sh
+++ wait_for_emulator.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+ANDROID_TARGET=android-18  
+ANDROID_ABI=armeabi-v7a
+
+# Create and start emulator
+echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
+emulator -avd test -no-skin -no-audio -no-window &
 
 bootanim=""
 failcounter=0
