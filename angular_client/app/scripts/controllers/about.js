e62d8f7b8060cd536d6e9a80234bdf56e47f37cb
cesine <cesine@yahoo.com>
2014-08-01 11:59:20 -0400

    updating karma test config to have all dependancies to be able to build the dist

--- angular_client/app/scripts/controllers/about.js
+++ angular_client/app/scripts/controllers/about.js
@@ -8,10 +8,6 @@
  * Controller of the adminDashboardApp
  */
 angular.module('adminDashboardApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function () {
+
   });
