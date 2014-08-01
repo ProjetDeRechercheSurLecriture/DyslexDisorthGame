3114d035f4eca599935a8bd562521f9acbd0dd9e
cesine <cesine@yahoo.com>
2014-08-01 11:59:38 -0400

    tests weren't checked in

---
+++ angular_client/test/spec/controllers/about.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: AboutCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('adminDashboardApp'));
+
+  var AboutCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    AboutCtrl = $controller('AboutCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should have an empty scope', function () {
+    expect(scope.awesomeThings).toBeUndefined();
+  });
+});
