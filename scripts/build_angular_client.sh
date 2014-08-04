3621d39b7a3923d380242289ca30cf640ab21694
cesine <cesine@yahoo.com>
2014-08-04 17:31:32 -0400

    travis builds depend on current source of fielddb-angular

--- scripts/build_angular_client.sh
+++ scripts/build_angular_client.sh
@@ -9,12 +9,13 @@ npm install || exit 1;
 bower install || exit 1;
 
 echo "Cloning and building a local fielddb-angular commonjs based on most recent commit";
-ls bower_components/fielddb-angular/dist/scripts/scripts.js || {
+# ls bower_components/fielddb-angular/dist/scripts/scripts.js || {
 	cd bower_components &&
 	git clone https://github.com/cesine/FieldDB.git &&
 	mv FieldDB fielddb-angular && 
 	cd fielddb-angular && 
 	npm install &&
-	grunt travis 
-} &&
+	grunt travis &&
+	cd $CURRENTDIR/angular_client
+# } &&
 grunt
