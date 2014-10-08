a3d264790f15fd10be873bc6dd515819875a9f3f
cesine <cesine@yahoo.com>
2014-10-08 12:25:18 -0400

    sample students file is available in deploy

--- angular_client/Gruntfile.js
+++ angular_client/Gruntfile.js
@@ -329,6 +329,7 @@ module.exports = function(grunt) {
             '*htaccess',
             '*.html',
             'jouer/*/**',
+            'sample/*.csv',
             'views/{,*/}*.html',
             'assets/*/*.mp3',
             'assets/*/*.png',
