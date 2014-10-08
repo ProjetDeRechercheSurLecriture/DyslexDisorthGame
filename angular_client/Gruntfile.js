5966f7dace7ae348bf4ccf6ea4e3e5ea01200a28
cesine <cesine@yahoo.com>
2014-10-07 20:36:38 -0400

    working .htaccess file for html5 url routing

--- angular_client/Gruntfile.js
+++ angular_client/Gruntfile.js
@@ -326,7 +326,7 @@ module.exports = function(grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
+            '*htaccess',
             '*.html',
             'jouer/*/**',
             'views/{,*/}*.html',
