a5fc35ed092310c113aaa57df808877842e81131
cesine <cesine@yahoo.com>
2014-12-01 17:19:50 -0500

    adding some of the travis config from dyslex disort and updating android sdk in setup scripts

--- scripts/set_up_new_machine.sh
+++ scripts/set_up_new_machine.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-export ANDROID_SDKS=android-18,sysimg-18 
-export ANDROID_TARGET=android-18  
+export ANDROID_SDKS=android-20,sysimg-20 
+export ANDROID_TARGET=android-20  
 export ANDROID_ABI=armeabi-v7a
 
 export PATH=${PATH}:/usr/local/bin
-npm install grunt
-npm install grunt-exec
-npm install grunt-contrib-jshint
-npm install grunt-contrib-watch
-npm install grunt-contrib-copy
-
+# npm install grunt-cli -g
+# npm install bower -g
+# npm install
 
 if [ "$(uname)" == "Darwin" ]; then {
     # If the git user name isnt set, use jenkins      
@@ -20,7 +17,7 @@ if [ "$(uname)" == "Darwin" ]; then {
     export ANDROID_HOME=$HOME/android-adt-bundle/sdk
     PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 } elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then {
-    sudo npm install -g grunt-cli
+    # sudo npm install -g grunt-cli
 
     # Do something under Linux platform
     sudo apt-get update -qq
@@ -29,7 +26,7 @@ if [ "$(uname)" == "Darwin" ]; then {
     tar xzf android-sdk_r22.0.5-linux.tgz
     export ANDROID_HOME=$PWD/android-sdk-linux
     export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
-    echo y | android update sdk --filter platform-tools,build-tools-18.0.1,android-18,sysimg-18 --no-ui --force > /dev/null
+    echo y | android update sdk --filter platform-tools,build-tools-20.0.1,android-20,sysimg-20 --no-ui --force > /dev/null
 } elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then {
     # Do something under Windows NT platform
     echo "You can fill in the Windows instructions to get android."
@@ -37,4 +34,5 @@ if [ "$(uname)" == "Darwin" ]; then {
 fi
 
 # Download all the code and updates for the first time
-grunt update;
+# cd ../
+# git clone https://github.com/ProjetDeRechercheSurLecriture/dyslex-disorth-game-sikuli.git
