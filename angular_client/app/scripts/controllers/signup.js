2709af4205aeea8cbe47fd8d20408718a52af5fd
cesine <cesine@yahoo.com>
2015-03-29 09:02:55 +0700

    added signup controller

---
+++ angular_client/app/scripts/controllers/signup.js
@@ -0,0 +1,45 @@
+/* globals FieldDB */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adminDashboardApp.controller:SignupController
+ * @description
+ * # SignupController
+ * Controller of the adminDashboardApp
+ */
+angular.module('adminDashboardApp')
+  .controller('SignupController', function($scope, $rootScope) {
+
+    $scope.registerDetails = {
+      firstname: '',
+      lastname: ''
+    };
+
+    $scope.baseUsernameOnFirstAndALastName = function() {
+      $scope.registerDetails.username = $scope.registerDetails.firstname + $scope.registerDetails.lastname;
+
+      if (FieldDB && FieldDB.CorpusConnection) {
+        $scope.registerDetails.username = FieldDB.CorpusConnection.validateIdentifier(
+          $scope.registerDetails.username
+        ).identifier;
+      }
+      $scope.userIsRegistering = true;
+    };
+    $scope.validateRegistrationDetails = function() {
+      $scope.registerDetails.firstname = $scope.registerDetails.firstname || '';
+      $scope.registerDetails.lastname = $scope.registerDetails.lastname || '';
+      $scope.registerDetails.email = $scope.registerDetails.email || '';
+
+      $scope.registerDetails.firstname = $scope.registerDetails.firstname.trim();
+      $scope.registerDetails.lastname = $scope.registerDetails.lastname.trim();
+      $scope.registerDetails.email = $scope.registerDetails.email.trim();
+
+      if (!$scope.registerDetails.firstname || !$scope.registerDetails.lastname || !$scope.registerDetails.email) {
+        return;
+      }
+
+      $rootScope.register($scope.registerDetails);
+    };
+
+  });
